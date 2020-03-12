dd39322ca6a50ab5264c9890271b10f7e4e4c46a
Miguel Gamboa <miguelgamboa@outlook.com>
2020-03-12 11:32:19 +0000

    Move anonymous to IteratorInputStream

--- aula07-lazy-reader/src/main/java/pt/isel/mpd/util/AbstractRequest.java
+++ aula07-lazy-reader/src/main/java/pt/isel/mpd/util/AbstractRequest.java
@@ -1,52 +1,11 @@
 package pt.isel.mpd.util;
 
-import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.NoSuchElementException;
 
 public abstract class AbstractRequest implements Request {
     public abstract InputStream getStream(String path);
     @Override
     final public Iterable<String> getLines(String path) {
-        return () -> new Iterator<String>() {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(getStream(path)));
-            String line;
-            boolean called;
-            @Override
-            public boolean hasNext() {
-                if(called) return line != null;
-                line = nextLine();
-                called = true;
-                if(line == null) closeReader();
-                return line != null;
-            }
-
-            private void closeReader() {
-                try {
-                    reader.close();
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-
-            private String nextLine() {
-                try {
-                    return reader.readLine();
-                } catch (IOException ex) {
-                    throw new RuntimeException(ex);
-                }
-            }
-            public String next() {
-                if(!hasNext()) throw new NoSuchElementException();
-                called = false;
-                return line;
-            }
-        };
+        return () -> new IteratorInputStream(getStream(path));
     }
 }

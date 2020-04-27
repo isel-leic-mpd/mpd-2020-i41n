a8d1f07098b2b8dacb1264622a154b93074c2f97
Miguel Gamboa <miguelgamboa@outlook.com>
2020-04-27 20:02:37 +0100

    Second version of Sequence

--- aula21-sequence-function/src/main/java/pt/isel/mpd/util/Sequence.java
+++ aula21-sequence-function/src/main/java/pt/isel/mpd/util/Sequence.java
@@ -1,7 +1,62 @@
 package pt.isel.mpd.util;
 
 import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public interface Sequence<T> {
     boolean tryAdvance(Consumer<T> action);
+
+    // Métodos Factory
+
+    static <T> Sequence<T> generate(Supplier<T> sup) {
+        /**
+         * Implementation of tryAdvance(Consumer) method.
+         */
+        return cons -> {
+            cons.accept(sup.get());
+            return true;
+        };
+    }
+
+    static <T> Sequence<T> of(T...args) {
+        BoxInt index = new BoxInt();
+        return cons -> {
+            if(index.val >= args.length) return false;
+            cons.accept(args[index.val++]);
+            return true;
+        };
+    }
+    // Operações intermédias
+    default Sequence<T> limit(int size) {
+        BoxInt index = new BoxInt();
+        return cons ->  index.val++ >= size
+            ? false
+            : this.tryAdvance(cons);
+    }
+    default Sequence<T> filter(Predicate<T> pred) {
+        return cons -> {
+            BoxBool found = new BoxBool();
+            while(!found.val && this.tryAdvance(item -> {
+                if(pred.test(item)) {
+                    found.val = true;
+                    cons.accept(item);
+                }
+            })) { }
+            return found.val;
+        };
+    }
+    default <R> Sequence<R> map(Function<T, R> mapper) {
+        return cons -> tryAdvance(item -> cons.accept(mapper.apply(item)));
+    }
+    // Operações Terminais
+    default void forEach(Consumer<T> cons) {
+        while(this.tryAdvance(cons)) {}
+    }
+
+
+    static class BoxInt { int val; }
+    static class BoxBool { boolean val; }
+
 }
--- aula21-sequence-function/src/test/java/pt/isel/mpd/SequenceQueriesTest.java
+++ aula21-sequence-function/src/test/java/pt/isel/mpd/SequenceQueriesTest.java
@@ -1,21 +1,14 @@
 package pt.isel.mpd;
 
 import org.junit.Test;
-import pt.isel.mpd.util.LazyQueries;
 import pt.isel.mpd.util.Sequence;
-import pt.isel.mpd.util.SequenceQueries;
 
-import java.util.Arrays;
-import java.util.Iterator;
 import java.util.Random;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static pt.isel.mpd.util.SequenceQueries.forEach;
-import static pt.isel.mpd.util.SequenceQueries.of;
-import static pt.isel.mpd.util.SequenceQueries.filter;
-import static pt.isel.mpd.util.SequenceQueries.limit;
-import static pt.isel.mpd.util.SequenceQueries.generate;
+import static pt.isel.mpd.util.Sequence.generate;
+import static pt.isel.mpd.util.Sequence.of;
 
 public class SequenceQueriesTest {
 
@@ -30,12 +23,15 @@ public class SequenceQueriesTest {
 
     int init;
     @Test public void testGenerateEvenIntegers() {
-        Sequence<Integer> expected = of(0, 2, 4, 6);
+        Sequence<String> expected = of("0", "2", "4", "6");
         init = 0;
-        Sequence<Integer> actual = limit(filter(generate(() -> init++), n -> n % 2 == 0), 4);
-        forEach(actual, curr -> expected.tryAdvance(other -> assertEquals(other, curr)));
+        Sequence
+            .generate(() -> init++)
+            .filter(n -> n % 2 == 0)
+            .limit(4)
+            .map(String::valueOf)
+            .forEach(curr -> expected.tryAdvance(other -> assertEquals(other, curr)));
         assertFalse(expected.tryAdvance(__ -> {}));
-        assertFalse(actual.tryAdvance(__ -> {}));
     }
 
 }

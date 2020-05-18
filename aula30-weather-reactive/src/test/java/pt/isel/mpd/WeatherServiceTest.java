a261ef00aef82fdfb60f830a9da5a1bf49434e81
Miguel Gamboa <miguelgamboa@outlook.com>
2020-05-18 15:22:50 +0100

    Setup aula 30

---
+++ aula30-weather-reactive/src/test/java/pt/isel/mpd/WeatherServiceTest.java
@@ -0,0 +1,30 @@
+package pt.isel.mpd;
+
+import org.junit.Test;
+import pt.isel.mpd.util.AsyncMockRequest;
+import pt.isel.mpd.weather.AsyncWeatherRestful;
+import pt.isel.mpd.weather.AsyncWeatherService;
+import pt.isel.mpd.weather.model.Weather;
+
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Stream;
+
+import static java.time.LocalDate.of;
+import static org.junit.Assert.assertEquals;
+
+public class WeatherServiceTest {
+    @Test public void testPastWeatherOfFaroCity() {
+        AsyncWeatherService service = new AsyncWeatherService(new AsyncWeatherRestful(new AsyncMockRequest()));
+        CompletableFuture<Stream<Weather>> jan = service
+            .search("Faro")                 // CF<Stream<Location>>
+            .thenApply(Stream::findFirst)   // CF<Optional<Location>>
+            .thenApply(Optional::get)       // CF<Location>
+            .thenCompose(faro -> faro.pastWeather(of(2020, 1, 1), of(2020, 1, 30))); // CF<Stream<Wearther>>>
+        jan
+            .thenApply(strm -> strm.map(Weather::getTempC).max(Integer::compare))
+            .thenApply(Optional::get)
+            .thenAccept(maxTempJan -> assertEquals(17, maxTempJan.intValue()))
+            .join();
+    }
+}
